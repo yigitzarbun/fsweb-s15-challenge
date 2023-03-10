3a33d0cbdf05fbde4fa6b10a37418f12299de751
Yigit Zarbun <yzarbun@gmail.com>
2023-03-10 14:07:33 +0300

    restricted calisti

--- api/bilmeceler/bilmeceler-router.js
+++ api/bilmeceler/bilmeceler-router.js
@@ -1,8 +1,9 @@
 // değişiklik yapmayın
-const router = require('express').Router();
-const bilmeceler = require('./bilmeceler-data');
+const router = require("express").Router();
+const bilmeceler = require("./bilmeceler-data");
+const restricted = require("../middleware/restricted");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json(bilmeceler);
 });
 
--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,4 +1,20 @@
+const { JWT_SECRET } = require("../secrets");
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decodedJWT) => {
+      if (err) {
+        res.status(401).json({ message: "token geçersizdir" });
+      } else {
+        req.userInfo = decodedJWT;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "token gereklidir" });
+  }
   next();
   /*
     EKLEYİN
